47ff2e19def521853c8fe8650424d943daafa3ab
Andrew Waters <pacific@tokyofish.com>
2026-01-14 20:46:47 -0500

    Initial Commit

---
+++ AppClipTest/AppClipTestApp.swift
@@ -0,0 +1,32 @@
+//
+//  AppClipTestApp.swift
+//  AppClipTest
+//
+//  Created by Andrew Waters on 1/14/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AppClipTestApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ AppClipTestTests/AppClipTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppClipTestTests.swift
+//  AppClipTestTests
+//
+//  Created by Andrew Waters on 1/14/26.
+//
+
+import Testing
+@testable import AppClipTest
+
+struct AppClipTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
